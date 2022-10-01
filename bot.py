1b0a849c49a66aaa7bf14a81e822f61976be802b
Kai Uerlichs <2421101@dundee.ac.uk>
2022-10-01 18:57:30 +0100

    Rudimentary log-on

--- bot.py
+++ bot.py
@@ -1,5 +1,12 @@
 import argparse
 import socket
+from time import sleep
+
+
+def prepare_message(message):
+    
+    bytes = message.encode("utf-8") + b'\r\n'
+    return bytes
 
 
 # Parse command-line arguments
@@ -13,3 +20,22 @@ argparser.add_argument("--channel", "-c", default="global", help="The channel fo
 args = argparser.parse_args()
 
 
+# Initialise and connect socket
+sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+try:
+    print("Attempting connection to", args.host, "at port", args.port)
+    sock.connect((args.host, int(args.port)))
+
+except:
+    print("Connection failed.")
+    quit()
+
+reg_string = ("NICK " + args.name).encode()
+reg_string += ("USER " + args.name + " 0 * :" + args.name).encode()
+
+sock.sendall(prepare_message("NICK " + args.name))
+sock.sendall(prepare_message("USER " + args.name + " 0 * :" + args.name))
+
+sleep(10)
+
+sock.close()
