3506a769f2413170673aac1229eb1f632dd0eea5
Kai Uerlichs <2421101@dundee.ac.uk>
2022-10-12 11:04:34 +0100

    Create API module

---
+++ utils/jokes.py
@@ -0,0 +1,22 @@
+""" Pulls jokes from a web API """
+
+import requests
+
+
+class APIException(Exception):
+    pass
+
+
+API_URL = "https://official-joke-api.appspot.com/jokes/programming/random"
+
+
+def get():
+    try:
+        res = requests.get(API_URL)
+        res.raise_for_status()
+    except:
+        raise APIException("Could not retrieve joke")
+
+    data = res.json()
+
+    return (data[0]["setup"], data[0]["punchline"])
